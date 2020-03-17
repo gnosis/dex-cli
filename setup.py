1e26ff9973c60e05aff4e466e3a5e49e1d7fd12a
Anxo Rodriguez <anxolin@gmail.com>
2020-03-17 16:13:38 +0100

    Upload license

--- setup.py
+++ setup.py
@@ -1,25 +1,29 @@
-from setuptools import setup
+from setuptools import setup, setuptools
 
 requirements = [
     "Click>=7.0",
     "gql>=0.3.0",
 ]
 
-# with open("README.md", 'r') as f:
-#     long_description = f.read()
+with open("README.md", 'r') as f:
+    long_description = f.read()
 
 setup(
-   name='dFusion',
-   version='0.1',
-   description='dFusion CLI',
-   #long_description=
-   long_description=long_description,
-   author='Gnosis',
-   author_email='admin@gnosis.pm',
-   keywords=['dex', 'defi', 'exchange', 'ethereum', 'dfusion', 'gnosis'],
-   packages=[
-     'src'
-    ],
-   install_requires=requirements,
-   url='https://github.com/gnosis/dex-cli',
+  name='dFusion',
+  version='0.1',
+  description='dFusion CLI',
+  license='MIT License',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author='anxolin',
+  author_email='anxolin@gmail.com',
+  keywords=['dex', 'defi', 'exchange', 'ethereum', 'dfusion', 'gnosis'],
+  packages=setuptools.find_packages(),
+  install_requires=requirements,
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  url='https://github.com/gnosis/dex-cli',
 )
