21bc880d391ebdde6ae3a08f1ce45d7b0f48043f
Anxo Rodriguez <anxolin@gmail.com>
2020-03-18 12:01:14 +0100

    Add order comand

--- src/cli.py
+++ src/cli.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from commands.orders import get_orders
 from commands.tokens import get_tokens
 from commands.trades import get_trades
 
@@ -37,6 +38,18 @@ def tokens(count, skip, sort, format, verbose):
 def trades(count, skip, sort, format, verbose, trader):
     """Get trades"""
     get_trades(count=count, skip=skip, sort=sort, format=format, verbose=verbose, trader=trader)
+
+
+@main.command()
+@click.option('--count', default=10, help='Number of orders to return, used for pagination')
+@click.option('--skip', default=0, help='Number of orders to skip, used for pagination')
+@click.option('--sort', default="fromBatchId", help='Sort result by a field, used for pagination')
+@click.option('--format', default="pretty", help='Format type i.e. pretty, csv')
+@click.option('-v', '--verbose', count=True)
+@click.option('--trader', help='Ethereum address of the trader')
+def orders(count, skip, sort, format, verbose, trader):
+    """Get orders"""
+    get_orders(count=count, skip=skip, sort=sort, format=format, verbose=verbose, trader=trader)
 
 
 if __name__ == "__main__":
