79f96c1e9bb1da712112d3ac65e5989e3779794e
Felix Leupold <felixleupold90@gmail.com>
2020-10-21 10:08:30 +0200

    Factoring networks in enum

--- src/constants.py
+++ src/constants.py
@@ -1,19 +1,43 @@
 from decimal import Decimal
+from enum import Enum
 import os
 
+class Network(Enum):
+  XDAI = 'xdai'
+  RINKEBY = 'rinkeby'
+  MAINNET = 'mainnet'
+
+  @classmethod
+  def from_env(cls):
+    if 'NETWORK' not in os.environ:
+      return Network.MAINNET
+    
+    if os.environ['NETWORK'] == "xdai":
+      return Network.XDAI
+    if os.environ['NETWORK'] == "rinkeby":
+      return Network.RINKEBY
+    if os.environ['NETWORK'] == "mainnet":
+      return Network.MAINNET
+    
+    raise RuntimeError("Unknown env value for Network: " + os.environ['NETWORK'])
+    
+
 # Datasource
-if 'NETWORK' in os.environ and os.environ['NETWORK'] == "xdai":
+network = Network.from_env()
+if network is Network.XDAI:
   URL_API_THE_GRAPH = 'https://api.thegraph.com/subgraphs/name/gnosis/protocol-xdai'
   URL_UI_THE_GRAPH = 'https://thegraph.com/explorer/subgraph/gnosis/protocol-xdai'
   TX_EXPLORER_BASE_URL = 'https://blockscout.com/poa/xdai'
-elif 'NETWORK' in os.environ and os.environ['NETWORK'] == "rinkeby":
+elif network is Network.RINKEBY:
   URL_API_THE_GRAPH = 'https://api.thegraph.com/subgraphs/name/gnosis/protocol-rinkeby'
   URL_UI_THE_GRAPH = 'https://thegraph.com/explorer/subgraph/gnosis/protocol-rinkeby'
   TX_EXPLORER_BASE_URL = 'https://rinkeby.etherscan.io'
-else:
+elif network is Network.MAINNET:
   URL_API_THE_GRAPH = 'https://api.thegraph.com/subgraphs/name/gnosis/protocol'
   URL_UI_THE_GRAPH = 'https://thegraph.com/explorer/subgraph/gnosis/protocol'
   TX_EXPLORER_BASE_URL = 'https://etherscan.io'
+else:
+  raise RuntimeError("Unknown Network")
 
 RETRIES = 3
 
